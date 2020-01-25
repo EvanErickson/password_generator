b24098bbcdb2e017895a228aa41fc67f962983f2
EvanErickson <evanserickson@gmail.com>
2020-01-24 21:32:48 -0800

    added some questions in comments

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let generateBtn = document.getElementById('generate');
 
 
 function makeid(length) {
+  //where did they get length from?
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$';
     var charactersLength = characters.length;
+    //I'm confused about why you'd set charactersLength = characters.length
     for ( var i = 0; i < length; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
